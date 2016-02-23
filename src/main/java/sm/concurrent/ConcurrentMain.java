18262e760bd7e12bd81181eeed6376839dde07d4
Suvankar Mazumder <smazumder@gmail.com>
2016-02-23 11:35:32 -0500

    Update ConcurrentMain.java

--- src/main/java/sm/concurrent/ConcurrentMain.java
+++ src/main/java/sm/concurrent/ConcurrentMain.java
@@ -1,7 +1,7 @@
 /**
  * 
  */
-package sm.main;
+package sm.concurrent;
 
 /**
  * @author smazumder6
@@ -9,30 +9,31 @@ package sm.main;
  */
 import com.bb.sm.concurrent.*;
 
-public class MainApp {
+public final class ConcurrentMain{
 
 	/**
 	 * The difference between  Normal Executor Service and Completion Service is visible at runtime, Normal Executor Service always waits  
 	 * for the next thread to finish the job before printing the number, the client needs to wait until a big bunch of threads return all together.
-     * Completion Service is behaving differently, it follows a producer/consumer philosophy: as soon a thread is done, it puts the result into a 
-     * non blocking queue so that the consumer can take it.
+         * Completion Service is behaving differently, it follows a producer/consumer philosophy: as soon a thread is done, it puts the result into a 
+         * non blocking queue so that the consumer can take it.
+         * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-       //System.out.println("Running Normal Loop");
-       long  start= System.currentTimeMillis();
-       System.out.println("Running Normal Loop");
-       new Completion().useNormalLoop();
-       System.out.println();
-       System.out.println("Execution time : " + (System.currentTimeMillis() - start));
-       System.out.println("Running Normal Executor");
-       new Completion().normalExecutor();
-       System.out.println();
-       System.out.println("Execution time : " + (System.currentTimeMillis() - start));
-       System.out.println("Running Completion Executor");
-       new Completion().completionService();
-       System.out.println();
-       System.out.println("Execution time : " + (System.currentTimeMillis() - start));
-	}
+          //System.out.println("Running Normal Loop");
+          long  start= System.currentTimeMillis();
+          System.out.println("Running Normal Loop");
+          new Completion().useNormalLoop();
+          System.out.println();
+          System.out.println("Execution time : " + (System.currentTimeMillis() - start));
+          System.out.println("Running Normal Executor");
+          new Completion().normalExecutor();
+          System.out.println();
+          System.out.println("Execution time : " + (System.currentTimeMillis() - start));
+          System.out.println("Running Completion Executor");
+          new Completion().completionService();
+          System.out.println();
+          System.out.println("Execution time : " + (System.currentTimeMillis() - start));
+       }
 }
